778d53fd737ee59b521f604db3f1ff32852f1ef2
Mingze Ma <mingzema.1998@foxmail.com>
2023-12-11 16:03:46 +0800

    feat: server actions, form components

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,21 @@
-const OrganizationIdPage = () => {
+import { db } from "@/lib/db";
 
-  return ( 
+import { Board } from "./board";
+import { Form } from "./form";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
     <div>
-      org page
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} {...board} />
+        ))}
+      </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default OrganizationIdPage;
