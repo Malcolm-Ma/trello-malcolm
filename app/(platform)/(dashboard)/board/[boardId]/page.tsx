52a98c8bfe4f6ad9f84b939dd4c55a6ca7ee5213
Mingze Ma <mingzema.1998@foxmail.com>
2023-12-13 22:53:23 +0800

    feat: create list

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,46 @@
-const BoardIdPage = () => {
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+const BoardIdPage = async ({ params: { boardId } }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
   return (
-    <div>
-      board
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer
+        boardId={boardId}
+        data={lists}
+      />
     </div>
   );
 };
