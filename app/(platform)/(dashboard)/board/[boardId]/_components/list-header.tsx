49268e888211830d3d976089a7c0d0e3cb30ef82
Mingze Ma <mingzema.1998@foxmail.com>
2023-12-28 18:08:57 +0800

    feat: create cards

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -8,14 +8,15 @@ import { toast } from "sonner";
 import { FormInput } from "@/components/form/form-input";
 import { useAction } from "@/hooks/use-action";
 import { updateList } from "@/actions/update-list";
-import { exec } from "child_process";
+
 import { ListOptions } from "./list-options";
 
 interface ListHeaderProps {
   data: List;
+  onAddCard: () => void;
 }
 
-export const ListHeader = ({ data }: ListHeaderProps) => {
+export const ListHeader = ({ data, onAddCard }: ListHeaderProps) => {
   const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -91,10 +92,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
           {title}
         </div>
       )}
-      <ListOptions
-        data={data}
-        onAddCard={() => {}}
-      />
+      <ListOptions data={data} onAddCard={onAddCard} />
     </div>
   );
 };
