b123351902637001d153d8ee4c57440173e33794
Mingze Ma <mingzema.1998@foxmail.com>
2024-01-03 18:16:55 +0800

    feat: card details

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -2,6 +2,7 @@
 
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
+import { useCardModal } from "@/hooks/use-card-modal";
 
 interface CardItemProps {
   data: Card;
@@ -12,6 +13,7 @@ export const CardItem = ({
   data,
   index,
 }: CardItemProps) => {
+  const cardModal = useCardModal();
 
   return (
     <Draggable draggableId={data.id} index={index}>
@@ -21,7 +23,7 @@ export const CardItem = ({
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
-          onClick={() => {}}
+          onClick={() => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
         >
           {data.title}
