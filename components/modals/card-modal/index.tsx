489c7a7139146bb2523332537056554e4292d87e
Mingze Ma <mingzema.1998@foxmail.com>
2024-01-09 21:21:37 +0800

    feat: card detail

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -2,11 +2,16 @@
 
 import { useQuery } from "@tanstack/react-query";
 
-import { Dialog, DialogContent } from "@/components/ui/dialog";
-import { useCardModal } from "@/hooks/use-card-modal";
-import { fetcher } from "@/lib/fetcher";
 import { CardWithList } from "@/types";
+import { fetcher } from "@/lib/fetcher";
+// import { AuditLog } from "@prisma/client";
+import { useCardModal } from "@/hooks/use-card-modal";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+
 import { Header } from "./header";
+import { Description } from "./description";
+import { Actions } from "./actions";
+// import { Activity } from "./activity";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -18,6 +23,11 @@ export const CardModal = () => {
     queryFn: () => fetcher(`/api/cards/${id}`),
   });
 
+  // const { data: auditLogsData } = useQuery<AuditLog[]>({
+  //   queryKey: ["card-logs", id],
+  //   queryFn: () => fetcher(`/api/cards/${id}/logs`),
+  // });
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
@@ -25,16 +35,19 @@ export const CardModal = () => {
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
           <div className="col-span-3">
             <div className="w-full space-y-6">
-              {/* {!cardData
-                ? <Description.Skeleton />
-                : <Description data={cardData} />
-              } */}
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+              {/* {!auditLogsData ? (
+                <Activity.Skeleton />
+              ) : (
+                <Activity items={auditLogsData} />
+              )} */}
             </div>
           </div>
-          {/* {!cardData
-            ? <Actions.Skeleton />
-            : <Actions data={cardData} />
-          } */}
+          {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
         </div>
       </DialogContent>
     </Dialog>
